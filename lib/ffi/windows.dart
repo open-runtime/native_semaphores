c204c592b3cabd61a68f4b9e449f35655c6763e9
Tsavo Knott <tsavo@pieces.app>
2024-04-08 20:06:18 -0400

    Update min and max to be 1 and 2 for wait for single object to not hang

--- lib/ffi/windows.dart
+++ lib/ffi/windows.dart
@@ -217,9 +217,9 @@ class WindowsCreateSemaphoreWMacros {
   // ERROR_SEM_IS_SET: The semaphore is already set, and cannot be set again.
   static const int ERROR_SEM_IS_SET = 102;
 
-  static int INITIAL_VALUE_RECOMMENDED = 0;
+  static int INITIAL_VALUE_RECOMMENDED = 1;
 
-  static int MAXIMUM_VALUE_RECOMMENDED = 1;
+  static int MAXIMUM_VALUE_RECOMMENDED = 2;
 
   static String GLOBAL_NAME_PREFIX = 'Global\\';
 
